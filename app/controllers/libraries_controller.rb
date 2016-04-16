f06b258775a0863d2823b28dd6d9940f978d5434
tnsherpa1 <tnsherpa1@gmail.com>
2016-04-16 10:39:26 -0700

    sets up library_user associations

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -7,4 +7,10 @@ class LibrariesController < ApplicationController
     @library = Library.new
     render :new
   end
+  def create
+    library_params = params.require(:library).permit(:name, :floor_count, :floor_area)
+    @library = Library.create(library_params)
+
+    redirect_to "/libraries"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   get "/libraries", to: "libraries#index"
   get "/libraries/new", to: "libraries#new"
+  post "/libraries", to: "libraries#create"
 end
