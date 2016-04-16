4b943dabe311f887e7b018b340d64ceb86aecc83
tnsherpa1 <tnsherpa1@gmail.com>
2016-04-15 21:37:00 -0700

    adds bcrypt to new user sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
     @user = User.new
     render :new
   end
+  def create
+    user_params = params.require(:user).permit(:first_name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to "/users"
+  end
 end
